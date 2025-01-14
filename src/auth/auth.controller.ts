a45d8ecd48bef38c4793c6cfe1b74acdd4d42b97
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-01-14 18:15:59 +0300

    create authorization registration and logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,9 +3,13 @@ import {
   Controller,
   HttpCode,
   Post,
+  Req,
+  Res,
+  UnauthorizedException,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common'
+import { Request, Response } from 'express'
 import { AuthDto } from 'src/dto/auth.dto'
 import { AuthService } from './auth.service'
 
@@ -15,8 +19,54 @@ export class AuthController {
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
+  @Post('login')
+  async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.login(dto)
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+
+    return response
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
   @Post('register')
-  async register(@Body() dto: AuthDto) {
-    return this.authService.register(dto)
+  async register(
+    @Body() dto: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.register(dto)
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+
+    return response
+  }
+
+  @HttpCode(200)
+  @Post('login/access-token')
+  async getNewTokens(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshTokenFromCookies =
+      req.cookies[this.authService.REFRESH_TOKEN_NAME]
+
+    if (!refreshTokenFromCookies) {
+      this.authService.removeRefreshTokenFromResponse(res)
+      throw new UnauthorizedException('Refresh token not passed')
+    }
+
+    const { refreshToken, ...response } = await this.authService.getNewTokens(
+      refreshTokenFromCookies,
+    )
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+
+    return response
+  }
+
+  @HttpCode(200)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenFromResponse(res)
+    return true
   }
 }
